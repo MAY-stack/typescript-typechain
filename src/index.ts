0eae1b7973a06499fcc772b4fdae7fdb2e685f1f
Marisol <s.min.g7904@gamail.com>
2023-12-19 17:38:16 +0900

    Create simple hash chain

--- src/index.ts
+++ src/index.ts
@@ -19,5 +19,37 @@ class Block implements BlockShape {
 
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    // return this.blocks;
+    return [...this.blocks];
+  }
+}
+
+const blockChain = new BlockChain();
+
+blockChain.addBlock("First one");
+blockChain.addBlock("Second one");
+blockChain.addBlock("Third one");
+
+console.log(blockChain.getBlocks());
